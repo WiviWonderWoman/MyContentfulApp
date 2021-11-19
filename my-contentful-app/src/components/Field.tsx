b0a7987a7532a9dff342034322e1dd9f420829bc
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-11-19 09:17:23 +0100

    removed unused code + commented in Field.tsx

--- my-contentful-app/src/components/Field.tsx
+++ my-contentful-app/src/components/Field.tsx
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { PlainClientAPI } from 'contentful-management';
-import { Paragraph, Grid, GridItem  } from '@contentful/forma-36-react-components';
+import { Grid, GridItem  } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
   cma: PlainClientAPI;
 }
+// get the fields by id
 const CONTENT_FIELD_ID = {column: 'columns', row: 'rows', data: 'data'};
 
 const Field = (props: FieldProps) => {
+
+  // The sdk allows interaction with the Contentful web app
   const { sdk } = props;
 
+  // reference individual fields from an entry with the field ID 
   const columnField = sdk.entry.fields[CONTENT_FIELD_ID.column];
   const rowField = sdk.entry.fields[CONTENT_FIELD_ID.row];
   const dataField = sdk.entry.fields[CONTENT_FIELD_ID.data];
 
+  // Get the current value from the fields and store it in React state
   const [gridData, setGridData] = useState(
     {
       columns: columnField.getValue(),
@@ -26,11 +31,11 @@ const Field = (props: FieldProps) => {
   useEffect(() => {
     const detachCol = columnField.onValueChanged((value) => {
       setGridData({columns: value, rows: rowField.getValue()});
-      console.log('column: ', gridData);
+      // console.log('column: ', gridData);
     });
     const detachRow = rowField.onValueChanged((value) => {
       setGridData({columns: columnField.getValue(), rows: value});
-      console.log('row: ',gridData);
+      // console.log('row: ',gridData);
     });
     return () => {
       detachCol();
@@ -38,19 +43,24 @@ const Field = (props: FieldProps) => {
     };
   }, []);
 
+  // Listen for onClick event and update the JSON-object value
   const handleClick=(value: number) => {
     dataField.setValue({value: value});
   }
+
+  // Array for GridItems
   const gridItems= [];
+  // Variables for loop
   let rowCount=0;
   let columnCount=0;
   const total= gridData.columns*gridData.rows;
 
+  // Loop through columns and rows
   for (let index = 0; index < total; index++) {
     if (columnCount === gridData.columns) {
       columnCount = 0;
       rowCount++;
-    }
+    }// Add GridItem to array
     gridItems.push(<GridItem  key={index} columnStart={columnCount} columnEnd={columnCount+1} rowStart={rowCount} rowEnd={rowCount+1} style={{border: 'solid 1px black'}}><button onClick={() => handleClick(index)}>{index}</button></GridItem>);
     columnCount++;
   }
@@ -58,6 +68,7 @@ const Field = (props: FieldProps) => {
   return (
   <>
     <Grid columns={gridData.columns} rows={gridData.rows} >
+      {/* render all GridItems in the array */}
       {gridItems}
     </Grid>
   </>
